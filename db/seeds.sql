b3b242477183ddc4257f020b4f98ca60d20440af
EsmondKim <ekelectronicmail@gmail.com>
2021-01-11 20:26:57 -0600

    Adding db file with schema.sql and seeds.sql.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Juicy Lucy");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bluesy Lucy");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Walleye Burger");
+
+SELECT * FROM burgers;
