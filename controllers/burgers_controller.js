854261412f75f37aec1c58323af46e540ed65055
EsmondKim <ekelectronicmail@gmail.com>
2021-01-11 22:11:34 -0600

    Added router.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,10 @@
+let express = require("express");
+let burger = require("../models/burger.js");
+
+let router = express.Router();
+
+//Create the `router` for the app, 
+//and export the `router` at the end of your file.
+
+module.exports = router; 
+
---
+++ models/burger.js
@@ -0,0 +1,10 @@
+let orm = require("../config/orm.js");
+
+//Also inside `burger.js`, create the code that will call the 
+//ORM functions using burger specific input for the ORM.
+
+let burger = {
+
+}
+
+module.exports = burger;
