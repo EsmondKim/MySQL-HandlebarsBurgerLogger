76fa2ea468bb86de257e60bfac0ff8b7f566615c
EsmondKim <ekelectronicmail@gmail.com>
2021-01-14 00:17:51 -0600

    Added function to burger.js and burger.js.  Set up ORM functions.  Created functions in burgers_controller.  Began setting up handlebars forms and lists.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,27 @@ let express = require("express");
 let burger = require("../models/burger.js");
 
 let router = express.Router();
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data 
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
-//Create the `router` for the app, 
-//and export the `router` at the end of your file.
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "name"  
+], [
+    req.body.name
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
 
 module.exports = router; 
 
--- models/burger.js
+++ models/burger.js
@@ -5,29 +5,22 @@ let orm = require("../config/orm.js");
 
 let burger = {
 
-    var cat = {
-        all: function(cb) {
-          orm.all("cats", function(res) {
-            cb(res);
-          });
-        },
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+           cb(res);
+        });
+    },
         // The variables cols and vals are arrays.
-        create: function(cols, vals, cb) {
-          orm.create("cats", cols, vals, function(res) {
+        insertOne: function(cols, vals, cb) {
+          orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
           });
         },
-        update: function(objColVals, condition, cb) {
-          orm.update("cats", objColVals, condition, function(res) {
+        updateOne: function(objColVals, condition, cb) {
+          orm.updateOne("cats", objColVals, condition, function(res) {
             cb(res);
           });
         },
-        delete: function(condition, cb) {
-          orm.delete("cats", condition, function(res) {
-            cb(res);
-          });
-        }
-
 }
 
 module.exports = burger;
