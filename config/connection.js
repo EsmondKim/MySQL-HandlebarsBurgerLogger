9357e5441ac61e44179e33b69ca9f8842f8996df
EsmondKim <ekelectronicmail@gmail.com>
2021-01-18 23:22:19 -0600

    Added Heroku environment variable code to connection.js.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);  
+} 
+else { 
+  connection = mysql.createConnection({
     host: "localhost",
   
     port: 3306,
@@ -9,7 +14,8 @@ let connection = mysql.createConnection({
   
     password: "vegetable",
     database: "burgers_db"
-  });
+    });
+  };
 
   connection.connect(function(err) {
     if (err) throw err;
