4ed0ba7f7f52c61f075ffedc0452e6df56550591
EsmondKim <ekelectronicmail@gmail.com>
2021-01-18 21:32:40 -0600

    Added css and bootstrap styling.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -42,22 +42,5 @@ console.log("Test")
         }
       );
     });
-
 })
-  
-//     $(".delete-cat").on("click", function(event) {
-//       var id = $(this).data("id");
-  
-//       // Send the DELETE request.
-//       $.ajax("/api/cats/" + id, {
-//         type: "DELETE"
-//       }).then(
-//         function() {
-//           console.log("deleted cat", id);
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-//   });
   
